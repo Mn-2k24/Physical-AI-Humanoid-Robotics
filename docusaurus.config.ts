156d0a45229137c20860c56ec0e69b93e28aa998
Nizam ul din <nyouwriteroficail@gmail.com>
2025-12-07 09:29:52 +0500

    fix baseUrl for Vercel

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://mn-2k24.github.io',
+  url: 'https://physical-ai-humanoid-robotics-zeta.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
